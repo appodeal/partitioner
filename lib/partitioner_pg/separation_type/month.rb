ff0fff5f40c957b118d62e2459a2b2f2613d2d10
roman.chudov <roman.chudov@gmail.com>
2018-04-16 17:10:05 +0700

    Possibility of specify partitioning column added; creation indexes, unique indexes and named indexes added.

--- lib/partitioner_pg/separation_type/month.rb
+++ lib/partitioner_pg/separation_type/month.rb
@@ -12,12 +12,18 @@ module PartitionerPg
       def create_month_table(date=Date.today)
         date_start = date.at_beginning_of_month
         date_end = date.at_beginning_of_month.next_month
-        sql = "CREATE TABLE IF NOT EXISTS #{name_of_partition_table(date)} (
-               CHECK ( created_at >= DATE('#{date_start}') AND created_at < DATE('#{date_end}') )
+        partition_table_name = name_of_partition_table(date)
+        sql = "CREATE TABLE IF NOT EXISTS #{partition_table_name} (
+               CHECK ( #{parting_column} >= DATE('#{date_start}') AND #{parting_column} < DATE('#{date_end}') )
                ) INHERITS (#{table_name});"
         execute_sql(sql)
-        sql = "ALTER TABLE #{name_of_partition_table(date)} ADD PRIMARY KEY (id);"
+        sql = "ALTER TABLE #{partition_table_name} ADD PRIMARY KEY (id);"
         execute_sql(sql)
+
+        create_partition_indexes(partition_table_name)
+        create_partition_named_indexes(partition_table_name)
+        create_partition_unique_indexes(partition_table_name)
+        create_partition_named_unique_indexes(partition_table_name)
       end
 
       def drop_month_table(date=Date.today)
@@ -33,8 +39,8 @@ module PartitionerPg
              curM varchar(2);
              tbl varchar(30);
            BEGIN
-              select cast(DATE_PART('year', new.created_at) as varchar) into curY;
-              select lpad(cast(DATE_PART('month', new.created_at) as varchar), 2, '0') into curM;
+              select cast(DATE_PART('year', new.#{parting_column}) as varchar) into curY;
+              select lpad(cast(DATE_PART('month', new.#{parting_column}) as varchar), 2, '0') into curM;
               tbl := '#{table_name}_y' || curY || 'm' || curM;
               EXECUTE format('INSERT into %I values ($1.*);', tbl) USING NEW;
               return NEW;
@@ -72,8 +78,75 @@ module PartitionerPg
          DROP FUNCTION #{table_name}_delete_trigger();"
       end
 
+      def create_partition_indexes(partition_table_name)
+        custom_indexes = partition_table_indexes.presence
+        return unless custom_indexes
+
+        custom_indexes.each { |custom_index| create_custom_index(partition_table_name, custom_index) }
+      end
+
+      def create_partition_named_indexes(partition_table_name)
+        custom_indexes = partition_table_named_indexes.presence
+        return unless custom_indexes
+
+        custom_indexes.map{|name, custom_index|
+          index_name = "index_#{partition_table_name}_#{name}"
+          create_custom_named_index(partition_table_name, custom_index, index_name)
+        }
+      end
+
+      def create_partition_unique_indexes(partition_table_name)
+        custom_unique_indexes = partition_table_unique_indexes.presence
+        return unless custom_unique_indexes
+
+        custom_unique_indexes.each { |custom_index| create_custom_index(partition_table_name, custom_index, true) }
+      end
+
+      def create_partition_named_unique_indexes(partition_table_name)
+        custom_indexes = partition_table_named_unique_indexes.presence
+        return unless custom_indexes
+
+        custom_indexes.map{|name, custom_index|
+          index_name = "index_#{partition_table_name}_#{name}"
+          create_custom_named_index(partition_table_name, custom_index, index_name, true)
+        }
+      end
+
+      def create_custom_index(table_name, index_fields, is_unique = false)
+        ActiveRecord::Migration.add_index table_name, index_fields, unique: is_unique
+      end
+
+      def create_custom_named_index(table_name, index_fields, name, is_unique = false)
+        ActiveRecord::Migration.add_index table_name, index_fields, name: name, unique: is_unique
+      end
+
       def name_of_partition_table(date=Date.today)
         date.strftime("#{table_name}_y%Ym%m")
+      end
+
+      # Template method
+      # Column which will determine partition for row (must be date or datetime type). Default value is :created_at
+      def parting_column
+        :created_at
+      end
+
+      # Template method
+      def partition_table_indexes
+        #
+      end
+
+      def partition_table_named_indexes
+        #
+      end
+
+      # Template method
+      def partition_table_unique_indexes
+        #
+      end
+
+      # Template method
+      def partition_table_named_unique_indexes
+        #
       end
     end
   end
